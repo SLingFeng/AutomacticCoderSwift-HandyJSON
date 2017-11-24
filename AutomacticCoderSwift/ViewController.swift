4924502a3e3c8ea6a0e820d65d7e76334e63ab00
孙凌锋 <slflibra@gmail.com>
2017-11-24 16:15:34 +0800

    Initial Commit

---
+++ AutomacticCoderSwift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AutomacticCoderSwift
+//
+//  Created by 孙凌锋 on 2017/11/24.
+//  Copyright © 2017年 孙凌锋. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
