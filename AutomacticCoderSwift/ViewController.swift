5c098407a2c45b3a3c6a0d38178f91aca8bc717c
孙凌锋 <slflibra@gmail.com>
2017-11-29 14:51:20 +0800

    完成

--- AutomacticCoderSwift/ViewController.swift
+++ AutomacticCoderSwift/ViewController.swift
@@ -8,14 +8,292 @@
 
 import Cocoa
 
+enum JsonValueType : Int {
+    case kOther = -1
+    case kString = 0
+    case kNumber = 1
+    case kArray = 2
+    case kDictionary = 3
+    case kBool = 4
+}
+
 class ViewController: NSViewController {
 
+    
+//    var _templateSwift = NSMutableString(contentsOfFile: Bundle.main.path(forResource: "json_swift", ofType: "txt")!, encoding: NSUTF8StringEncoding.rawValue)
+    var _templateSwift : NSMutableString?
+    
+    var _path : String?
+    
+    
+    @IBOutlet weak var classNameTF: NSTextField!
+    @IBOutlet weak var sBtn: NSButton!
+    @IBOutlet var jsonTV: NSTextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
+    
+    @IBAction func sBtnClick(_ sender: Any) {
+        _templateSwift = NSMutableString(string: try! String(contentsOfFile: Bundle.main.path(forResource: "json_swift", ofType: "txt") ?? "", encoding: .utf8))
+        
+        if self.classNameTF.stringValue.count == 0 {
+            self.message("没有输入Class名！", "")
+            return
+        }
+        if self.jsonTV.string.count == 0 {
+            self.message("Json无数据", "")
+            return
+        }
+        //开始解析json
+        var jsonStr = self.jsonTV.string.replacingOccurrences(of: "“", with: "\"")
+        jsonStr = jsonStr.replacingOccurrences(of: "”", with: "\"")
+        
+        let response = jsonStr.data(using: .utf8)
+        let dict = (try? JSONSerialization.jsonObject(with: response!, options: .mutableLeaves)) as? [AnyHashable: Any]
+        
+        if (dict != nil) {
+            let panel = NSOpenPanel()
+            panel.canChooseDirectories = true;
+            panel.canChooseFiles = false
+            weak var weakSelf: ViewController? = self
+            panel.begin(completionHandler: { (_ result) -> Void in
+                
+//            })
+//            panel.beginSheet(NSApplication.shared.keyWindow!, completionHandler: {(_ result) -> Void in
+                if result.rawValue == 0 {
+                    return
+                }
+                weakSelf?._path = panel.url?.path
+                
+                weakSelf?.generateClass((weakSelf?.classNameTF.stringValue)!, dict as! Dictionary<NSString, Any>)
+                weakSelf?.writeToFile()
+            })
+            
+        }
+    }
+    
+    
+    func message(_ str : String, _ str2 :String) -> Void {
+        let string = str2.copy()
+        self.jsonTV.string = str;
+        self.jsonTV.textColor = .red
+        self.jsonTV.font = NSFont.systemFont(ofSize: 50)
+        self.sBtn.isEnabled = false
+        
+        
+        self.perform(#selector(self.normal), with: string, afterDelay: 1)
+    }
+    
+    @objc func normal(_ str : String) -> Void {
+        self.jsonTV.string = str;
+        self.jsonTV.textColor = .black
+        self.jsonTV.font = NSFont.systemFont(ofSize: 12)
+        self.sBtn.isEnabled = true
+    }
+    
+    
+    func generateClass(_ nameTemp: String, _ json: Dictionary<NSString, Any>) -> Void {
+        
+        let templateModelSwift = NSMutableString(string: try! String(contentsOfFile: Bundle.main.path(forResource: "oneModel_swift", ofType: "txt") ?? "", encoding: .utf8))
+        
+        //property
+        let property = NSMutableString()
+        
+        let prefix = self.classNameTF.stringValue
+        var name = nameTemp as String;
+        
+        if !name.isEqual(self.classNameTF.stringValue) {
+            name = "\(prefix)\(name)Model"
+        }else {
+            name = "\(name)Model"
+        }
+        
+        for key: NSString in json.keys {
+            print(key)
+            
+            let type = self.type(json[key] as Any) as JsonValueType
+            
+            switch (type) {
+            case .kString, .kBool:
+                property.append("var \(key) : \(typeName(type))?\n\n    ")
+                break
+                
+            case .kNumber:
+                property.append("var \(key) = \(typeName(type))())\n\n    ")
+                break
+                
+            case .kArray:
+                property.append("var \(key) : [\(prefix)\(uppercaseFirstChar(key))Model]\n\n    ")
+                let arr = json[key] as! NSArray
+                if arr.count != 0 {
+                    self.generateClass(uppercaseFirstChar(key), arr.object(at: 0) as! Dictionary<NSString, Any>)
+                }
+                break
+                
+            case .kDictionary:
+                property.append("var \(key) : \(prefix)\(uppercaseFirstChar(key))Model?\n\n    ")
+                let arr = json[key] as! NSArray
+                if arr.count != 0 {
+                    self.generateClass(uppercaseFirstChar(key), arr.object(at: 0) as! Dictionary<NSString, Any>)
+                }
+                break
+                
+            default:
+                break
+            }
+        }
+        
+        templateModelSwift.replaceOccurrences(of: "#name#", with: name, options: .caseInsensitive, range: NSRange(location: 0, length: templateModelSwift.length))
+        
+        templateModelSwift.replaceOccurrences(of: "#property#", with: property as String, options: .caseInsensitive, range: NSRange(location: 0, length: templateModelSwift.length))
+        
+        //写文件
+        _templateSwift?.append(templateModelSwift as String)
+    }
+    
+    func writeToFile() {
+        _templateSwift?.replaceOccurrences(of: "#name#", with: "\(self.classNameTF.stringValue)Model", options: .caseInsensitive, range: NSRange(location: 0, length: (self.classNameTF.stringValue as NSString).length))
 
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        
+        _templateSwift?.replaceOccurrences(of: "#time#", with: formatter.string(from: Date()), options: .caseInsensitive, range: NSRange(location: 0, length: (_templateSwift?.length)!))
+        
+        do {
+            try _templateSwift?.write(toFile: "\(_path!)/\(self.classNameTF.stringValue)Model.swift", atomically: false, encoding: String.Encoding.utf8.rawValue)
+//            var data = Data()
+//            data.append((_templateSwift?.data(using: String.Encoding.utf8.rawValue))!)
+//            try data.write(to: URL(fileURLWithPath: "\(_path!)/\(self.classNameTF.stringValue)Model.swift"))
+//            _templateSwift?.write(to: URL(fileURLWithPath: "\(_path!)/\(self.classNameTF.stringValue)Model.swift"), atomically: true, encoding: String.Encoding.utf8.rawValue)
+        } catch  {
+            
+        }
+//        if !fileManage.fileExistsAtPath(filePath!){
+//            let bundlePath = NSBundle.mainBundle().resourcePath
+//            let defaultFilePath = bundlePath! + "/\(NoteFile)"
+//            do{
+//                try fileManage.copyItemAtPath(defaultFilePath, toPath: filePath!)
+//            }catch{
+//                print("错误写入文件!")
+//            }
+//        }
+//        let fileManage = FileManager()
+//        let filePath = _path
+//        let uf = fileManage.urls(for: .documentDirectory, in: .userDomainMask)
+//        let url = uf[0]
+//        createFile(name: "\(_path!)/\(self.classNameTF.stringValue).swift", fileBaseUrl: url)
+//        if !fileManage.fileExists(atPath: filePath!) {
+//            fileManage.co
+//        }
+        
+    }
+    
+//    func createFile(name:String, fileBaseUrl:URL){
+//        let manager = FileManager.default
+//
+//        let file = fileBaseUrl.appendingPathComponent(name)
+//        print("文件: \(file)")
+//        let exist = manager.fileExists(atPath: file.path)
+//        if !exist {
+//            let data = Data(base64Encoded:"aGVsbG8gd29ybGQ=" ,options:.ignoreUnknownCharacters)
+//            let createSuccess = manager.createFile(atPath: file.path,contents:data,attributes:nil)
+//            print("文件创建结果: \(createSuccess)")
+//        }
+//    }
+    
+    func isDataArray(_ theArray: [Any]) -> Bool {
+        if theArray.count <= 0 {
+            return false
+        }
+        
+        for item: Any in theArray {
+            if self.type(item) != JsonValueType.kDictionary {
+                return false
+            }
+        }
+        
+        let keys = NSMutableSet()
+        let tempDic = theArray[0]
+        for key: String in tempDic as! Dictionary<String, Any>.Keys {
+            keys.add(key)
+        }
+        
+        for item: Any in theArray {
+            let newKeys = NSMutableSet()
+            
+            for key: String in item as! Dictionary<String, Any>.Keys {
+                newKeys.add(key)
+            }
+            
+            if keys.isEqual(to: newKeys) == false {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func type(_ obj: Any) -> JsonValueType {
+        if (obj is NSString) {
+            return JsonValueType.kString
+        }else if ((obj as AnyObject).className == "__NSCFBoolean") || obj is Bool {
+            return JsonValueType.kBool
+        }else if obj is [Any] {
+            return JsonValueType.kArray
+        }else if obj is [AnyHashable: Any] {
+            return JsonValueType.kDictionary
+        }else if obj is NSNumber {
+            return JsonValueType.kNumber
+        }else {
+            return JsonValueType.kString
+        }
+        
+        
+//        if (obj as AnyObject).className == "__NSCFString" || (obj as AnyObject).className == "__NSCFConstantString" {
+//            return JsonValueType.kString
+//        }else if (obj as AnyObject).className == "__NSCFNumber" {
+//            return JsonValueType.kString
+//        }else if (obj as AnyObject).className == "__NSCFBoolean" {
+//            return JsonValueType.kString
+//        }else if (obj as AnyObject).className == "JKDictionary" {
+//            return JsonValueType.kString
+//        }else if (obj as AnyObject).className == "__NSCFNumber" {
+//            return JsonValueType.kString
+//        }
+        
+        return JsonValueType.kOther
+    }
+    
+    func typeName(_ type: JsonValueType) -> String {
+        switch type {
+        case .kString:
+            return "NSString"
+            
+        case .kNumber:
+            return "NSNumber"
+            
+        case .kBool:
+            return "Bool"
+            
+        case .kArray, .kDictionary:
+            return ""
+            
+        default:
+            return ""
+//            break
+        }
+    }
+    
+    func uppercaseFirstChar(_ str: NSString) -> String {
+        return "\(str.substring(to: 1).uppercased())\(str.substring(with: NSRange(location: 1, length: str.length - 1)))"
+    }
+    
+    func lowercaseFirstChar(_ str: NSString) -> NSString {
+        return "\(str.substring(to: 1).lowercased())\(str.substring(with: NSRange(location: 1, length: str.length - 1)))" as NSString
+    }
+    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
